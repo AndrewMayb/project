c5ce17c5230ffbd7b084602b7e1e0a8d81774403
Andrey <example2@gmail.com>
2023-07-10 11:49:29 +0200

    FIrst

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// alert('Hello');
+
+// const result = confirm("Are you here ?");
+
+// console.log(result)
+
+const answer = []
+answer[0] = prompt('Kak vashe imja', '')
+answer[1] = prompt('Kak vasha familija', '')
+answer[2] = prompt('Skolko vam let', '')
+
+document.write(answer)
