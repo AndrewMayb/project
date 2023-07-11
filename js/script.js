808049936d5f95db46a8640d1bc605d3301782c4
Andrey <example2@gmail.com>
2023-07-11 10:42:44 +0200

    add script file

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-// alert('Hello');
+'use strict'
 
-// const result = confirm("Are you here ?");
+console.log(4 + 4 + + '5')
+let incr = 10,
+    decr = 10
 
-// console.log(result)
 
-const answer = []
-answer[0] = prompt('Kak vashe imja', '')
-answer[1] = prompt('Kak vasha familija', '')
-answer[2] = prompt('Skolko vam let', '')
+// incr++ 
+// decr--
 
-document.write(answer)
+console.log(++incr)
+console.log(--decr)
+
+console.log(2 * 4 == '9')
